2ef795596f53962e727ac596b41020ca10292a4d
asm_m <riy_m218@yahoo.co.jp>
2020-06-16 00:31:00 +0900

    タスク新規作成、編集、削除に関するテストを大きくログイン前、ログイン後に分けて記述

---
+++ spec/system/tasks_spec.rb
@@ -0,0 +1,137 @@
+require 'rails_helper'
+require 'support/driver_setting'
+require 'support/login_support'
+
+RSpec.describe "Tasks", type: :system do
+  include LoginSupport
+  let(:user) { FactoryBot.create(:user) }
+  let(:task) { FactoryBot.create(:task, user: user) }
+
+  describe 'ログイン前' do
+    describe 'タスク新規登録' do
+      context 'ログインしていない状態' do
+        it '未ログイン状態ではタスク新規作成ができない' do
+          # タスク新規作成ページに遷移
+          visit new_task_path
+          expect(page).to have_content 'Login required'
+        end
+      end
+    end
+    
+    describe 'タスク編集' do
+      context 'ログインしていない状態' do
+        it '未ログイン状態ではタスク編集ができない' do
+          # タスク編集ページに遷移
+          visit edit_task_path(task)
+          expect(page).to have_content 'Login required'
+        end
+      end
+    end
+  end
+
+  describe 'ログイン後' do
+    describe 'タスク新規登録' do
+      context 'フォームの入力値が正常' do
+        it 'タスクの新規登録が成功する' do
+          sign_in(user)
+          # タスク新規作成画面に遷移
+          visit new_task_path
+          fill_in 'task[title]', with: 'テストタイトル'
+          fill_in 'task[content]', with: '本文'
+          select 'todo', from: status
+          click_button 'Create Task'
+          expect(page).to have_content 'Task was successfully created.'
+        end
+      end
+
+      context 'タイトルが未入力' do
+        it 'タスクの新規作成が失敗する' do
+          sign_in(user)
+          visit new_task_path
+          # タイトルが未入力
+          fill_in 'task[title]', with: ''
+          select 'todo', from: status
+          click_button 'Create Task'
+          expect(page).to have_content "Title can't be blank"
+        end
+      end
+
+      context '登録済みのタイトルを使用' do
+        it 'タスクの新規作成が失敗する' do
+          sign_in(user)
+          visit new_task_path
+          # 既に存在しているタイトル(letで作られているデータ)を入力
+          fill_in 'task[title]', with: task.title
+          select 'todo', from: status
+          click_button 'Create Task'
+          expect(page).to have_content 'Title has already been taken'
+        end
+      end
+    end
+    
+    describe 'タスク編集' do
+      # ユーザーに紐づいたテストタスクを作成
+      context 'フォームの入力値が正常' do
+        it 'タスクの編集が成功する' do
+          sign_in(user)
+          visit edit_task_path(task)
+          fill_in 'task[title]', with: 'テストタイトル更新'
+          select 'todo', from: status
+          click_button 'Update Task'
+          expect(page).to have_content 'Task was successfully updated.'
+        end
+      end
+
+      context 'タイトルが未入力' do
+        it 'タスクの編集が失敗する' do
+          sign_in(user)
+          visit edit_task_path(task)
+          # タイトルが未入力
+          fill_in 'task[title]', with: ''
+          select 'todo', from: status
+          click_button 'Update Task'
+          expect(page).to have_content "Title can't be blank"
+        end
+      end
+
+      context '登録済のタイトルを使用' do
+        it 'タスクの編集が失敗する' do
+          sign_in(user)
+          visit edit_task_path(task)
+          # 別のタスクを作成
+          other_task = FactoryBot.create(:task)
+          # 既に存在しているタイトルを使用
+          fill_in 'task[title]', with: other_task.title
+          select 'todo', from: status
+          click_button 'Update Task'
+          expect(page).to have_content 'Title has already been taken'
+        end
+      end
+    end
+
+    describe 'タスク削除' do
+      # itが実行される前にデータを作成、保存
+      let!(:task) { FactoryBot.create(:task, user: user) }
+      it 'タスクの削除が成功する' do
+        sign_in(user)
+        visit tasks_path
+        click_link 'Destroy'
+        # タスク消去の確認ポップアップページ
+        expect(page.accept_confirm).to eq 'Are you sure?'
+        expect(page).to have_content 'Task was successfully destroyed.'
+      end
+    end
+
+    describe '他のユーザーのタスク編集ページにアクセス' do
+      # 別のユーザーを作成
+      other_user = FactoryBot.create(:user)
+      it '他ユーザーのタスク編集ページへのアクセスが失敗する' do
+        sign_in(user)
+        # 別のユーザーのタスクを作成
+        other_user_task = FactoryBot.create(:task, user: other_user)
+        visit edit_task_path(other_user_task)
+        expect(page).to have_content 'Forbidden access.'
+      end
+    end
+  end
+end
